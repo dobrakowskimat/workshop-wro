2f8dff15c6d44718b2f8ec886af9e127e49ddbaa
Elzbieta Watroba <elzbieta.watroba@avanade.com>
2020-12-02 20:08:26 +0100

    remove pl locale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { LOCALE_ID, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,7 +21,6 @@ import { HttpClientModule } from '@angular/common/http';
     FlexLayoutModule,
     MatButtonModule,
   ],
-  providers: [{ provide: LOCALE_ID, useValue: 'pl' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}

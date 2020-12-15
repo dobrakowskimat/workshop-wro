75f1682282893514979424b21926bb0230af68b7
M. Dobrakowski (c) <m.dobrakowski@avanade.com>
2020-12-15 16:30:40 +0100

    after 4th workshop

---
+++ src/app/services/books.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Book, BookPayload } from '../main/books/shared/models/book.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BooksService {
+  constructor(private httpClient: HttpClient) {}
+
+  apiUrl: string = environment.apiUrl;
+
+  getBooks(): Observable<Book[]> {
+    return this.httpClient.get<Book[]>(`${this.apiUrl}/books`);
+  }
+
+  addBook(book: BookPayload): Observable<Book> {
+    return this.httpClient.post<Book>(`${this.apiUrl}/books`, book);
+  }
+}

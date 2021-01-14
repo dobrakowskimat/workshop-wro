bc7d58bf7887fb17605a2fd916eb32380b69173d
Adam Dłubak <adam.dlubak@gmail.com>
2021-01-14 17:12:06 +0100

    Fixed navigation

--- src/app/core/navigation/navigation.component.ts
+++ src/app/core/navigation/navigation.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { View } from '../books/shared/enums';
+import { View } from '../../main/books/shared/enums';
 
 @Component({
   selector: 'app-navigation',

fad46309c5d0cbd46370e6f74f53dd5b6c444a99
M. Dobrakowski (c) <m.dobrakowski@avanade.com>
2021-02-18 19:31:41 +0100

    validator

--- src/app/main/books/edit-book/edit-book.component.ts
+++ src/app/main/books/edit-book/edit-book.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Book, BookPayload } from '../shared/models/book.model';
 import { Subscription } from 'rxjs';
 import { BooksService } from 'src/app/services/books.service';
+import { rangeValidator } from '../shared/validators/range-validator';
 
 @Component({
   selector: 'app-edit-book',
@@ -21,9 +22,7 @@ export class EditBookComponent implements OnInit {
       this.bookForm = this.fb.group({
         title: this.fb.control(this.selectedBook.title, [Validators.required]),
         subtitle: this.fb.control(this.selectedBook.subtitle),
-        originallyPublishedYear: this.fb.control(
-          this.selectedBook.originallyPublishedYear
-        ),
+        originallyPublishedYear: this.fb.control(this.selectedBook.originallyPublishedYear, rangeValidator(0, 2021)),
         seriesTitle: this.fb.control(this.selectedBook.seriesTitle),
         pageCount: this.fb.control(this.selectedBook.pageCount),
         description: this.fb.control(this.selectedBook.description),
@@ -33,7 +32,7 @@ export class EditBookComponent implements OnInit {
       this.bookForm = this.fb.group({
         title: this.fb.control('', [Validators.required]),
         subtitle: this.fb.control(''),
-        originallyPublishedYear: this.fb.control(''),
+        originallyPublishedYear: this.fb.control('', rangeValidator(0, 2021)),
         seriesTitle: this.fb.control(''),
         pageCount: this.fb.control(''),
         description: this.fb.control(''),
@@ -53,11 +52,7 @@ export class EditBookComponent implements OnInit {
   }
 
   editBook(book: BookPayload) {
-    this.subscription.add(
-      this.booksService
-        .editBook(book)
-        .subscribe(() => console.log('Book successfully updated'))
-    );
+    this.subscription.add(this.booksService.editBook(book).subscribe(() => console.log('Book successfully updated')));
   }
 
   ngOnDestroy() {
---
+++ src/app/main/books/shared/validators/range-validator.ts
@@ -0,0 +1,10 @@
+import { AbstractControl, ValidatorFn } from '@angular/forms';
+
+export function rangeValidator(min = 0, max = 1000): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: boolean } | null => {
+    if (control.value !== null && (isNaN(control.value) || control.value < min || control.value > max)) {
+      return { bookValidationFail: true };
+    }
+    return null;
+  };
+}

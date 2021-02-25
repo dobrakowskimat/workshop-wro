52554c8d2b103ea2fc615cf1c0bd35d5b28f86a6
M. Dobrakowski (c) <m.dobrakowski@avanade.com>
2021-02-25 18:42:51 +0100

    edit form

--- src/app/main/books/edit-book/edit-book.component.ts
+++ src/app/main/books/edit-book/edit-book.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Book, BookPayload } from '../shared/models/book.model';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { BooksService } from 'src/app/services/books.service';
 import { rangeValidator } from '../shared/validators/range-validator';
 import { ActivatedRoute } from '@angular/router';
+import { map, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-edit-book',
@@ -14,8 +15,9 @@ import { ActivatedRoute } from '@angular/router';
 export class EditBookComponent implements OnInit {
   bookForm: FormGroup;
   subscription = new Subscription();
+  editedBook$: Observable<Book>;
 
-  constructor(private activedRoute: ActivatedRoute,private fb: FormBuilder, private booksService: BooksService) {}
+  constructor(private activedRoute: ActivatedRoute, private fb: FormBuilder, private booksService: BooksService) {}
 
   ngOnInit(): void {
     this.bookForm = this.fb.group({
@@ -25,19 +27,28 @@ export class EditBookComponent implements OnInit {
       seriesTitle: this.fb.control(''),
       pageCount: this.fb.control(''),
       description: this.fb.control(''),
-      publishDate: this.fb.control(''),
     });
-    this.activedRoute;
-    // if (this.selectedBook != null) {
-    //   this.bookForm = this.fb.group({
-    //     title: this.fb.control(this.selectedBook.title, [Validators.required]),
-    //     subtitle: this.fb.control(this.selectedBook.subtitle),
-    //     originallyPublishedYear: this.fb.control(this.selectedBook.originallyPublishedYear, rangeValidator(0, 2021)),
-    //     seriesTitle: this.fb.control(this.selectedBook.seriesTitle),
-    //     pageCount: this.fb.control(this.selectedBook.pageCount),
-    //     description: this.fb.control(this.selectedBook.description),
-    //     publishDate: this.fb.control(this.selectedBook.originallyPublishedYear),
-    //   });
+    this.editedBook$ = this.activedRoute.paramMap.pipe(
+      switchMap((params) => {
+        console.log(params.get('id'));
+        return this.booksService.getBook(params.get('id'));
+      }),
+    );
+    this.subscription.add(
+      this.editedBook$.subscribe((editedBook) => {
+        if (editedBook !== undefined) {
+          this.bookForm.patchValue({
+            title: editedBook.title,
+            subtitle: editedBook.subtitle,
+            originallyPublishedYear: editedBook.originallyPublishedYear,
+            seriesTitle: editedBook.seriesTitle,
+            pageCount: editedBook.pageCount,
+            description: editedBook.description,
+          });
+          this.bookForm.updateValueAndValidity();
+        }
+      }),
+    );
   }
 
   onSubmit() {
